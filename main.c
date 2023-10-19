863c804152aaf266b261b1fc9aceb3500ffb1d24
Hyeri Park <phr66@naver.com>
2023-10-19 15:33:21 +0900

    02

--- main.c
+++ main.c
@@ -3,14 +3,34 @@
 
 int main(int argc, char *argv[])
 {
-  int flag = 1;
-  int y;
-  while(flag != 0)
+  int i; 
+  
+  for (i=0; i<5; i++)
   {
-     y = 3;
-     flag = 0;
+      int temp = 1;
+      printf("temp = %d\n",  temp);
+      temp ++;
   }
-  y = 4;
+
   system("PAUSE");	
   return 0;
 }
+
+/*
+< temp = 1 temp = 2 temp = 3 temp = 4 temp 5를 출력하려면? >
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int i; 
+  
+  for (i=0; i<5; i++)
+  {
+      static int temp = 1; //static으로 생존기간을 늘림
+      printf("temp = %d\n",  temp);
+      temp ++;
+  }
+  
+ */ 
