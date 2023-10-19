d3351f5f8641d58153b0404c5b5687b3958ea850
Hyeri Park <phr66@naver.com>
2023-10-19 16:12:20 +0900

    05

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f(void);
+void sub (void);
 
-int i; //전역 변수로 설정 : #이 10개가 찍힐 것 
 int main (int argc, char *argv[])
 {
-    //int i; (i를 지역변수로 설정  -> 10개의 #이 5번 반복되어 최종 50개 출력) 
-    for (i=0; i<5; i++)
-    {
-        f();
-    }
+    int i;
+    for (i=0; i<3; i++)
+        sub();
     system ("PAUSE");
     return 0;
 }
 
-void f (void)
+void sub (void)
 {
-     //int i; (i를 지역변수로 설정)
-     for (i=0; i<10; i++)
-         printf("#");
+     int auto_count = 0; //stack 영역  
+     static int static_count = 0; //data 영역 
+     auto_count++;
+     static_count++;
+     printf("auto_count=%d\n", auto_count);
+     printf("static_count=%d\n", static_count);
 }
 
 
